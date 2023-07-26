cc26b7070e39dd25367e5bb546126c253bd755ed
Akash <mahmudakash177@gmail.com>
2023-07-26 20:15:34 +0600

    ci cd integration and bug fix

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,23 +1,23 @@
+import store from '@/store';
 import {  ApolloClient, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from '@apollo/client/link/context';
+
 import fetch from 'cross-fetch';
 
-let token =''
-if (typeof window !== 'undefined')  {
-  let authState 
-  
-  try {
-    authState = localStorage.getItem('persist:root')? JSON.parse(JSON.parse(localStorage.getItem('persist:root') as string)?.auth):''
-  } catch (error) {
-    
-  }
-  
-  if(authState?.isAuthenticated){
-    token = 'Bearer'+' '+authState?.token
 
-  }
 
-};
+
+const authLink = setContext((_, { headers }) => {
+  const token = store.getState().auth.token;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 
 /**
  * @param httpLink  from @apollo/client should be replace with
@@ -25,20 +25,16 @@ if (typeof window !== 'undefined')  {
  */
 
 
-const uploadLink = createUploadLink({
+const uploadAbleHttpLink = createUploadLink({
   uri: process.env.NEXT_PUBLIC_SERVER_URL,
-  headers: {
-    Authorization: token,
-    'Apollo-Require-Preflight': 'true'
 
-  },
   fetch,
 
 });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: uploadLink
+  link: authLink.concat(uploadAbleHttpLink)
 
 });
 
